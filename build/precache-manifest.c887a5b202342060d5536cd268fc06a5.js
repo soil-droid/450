5f5522a6754a8171172e1739ead43138944b9611
soil-droid <71129242+soil-droid@users.noreply.github.com>
2023-02-17 16:02:51 +0530

    Add files via upload

---
+++ build/precache-manifest.c887a5b202342060d5536cd268fc06a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae975f85b579b3fd85e3e960d5a044b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a122ea8f3d64a2980564",
+    "url": "/static/css/2.03bdd725.chunk.css"
+  },
+  {
+    "revision": "4fa03d10cd3982779409",
+    "url": "/static/css/main.cf10245c.chunk.css"
+  },
+  {
+    "revision": "a122ea8f3d64a2980564",
+    "url": "/static/js/2.f7b99ddc.chunk.js"
+  },
+  {
+    "revision": "5467e851392549ae3564d75adbc67604",
+    "url": "/static/js/2.f7b99ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa03d10cd3982779409",
+    "url": "/static/js/main.51c42fd4.chunk.js"
+  },
+  {
+    "revision": "3b33fffe9e6ff119bba8",
+    "url": "/static/js/runtime-main.4fb6a82e.js"
+  }
+]);
